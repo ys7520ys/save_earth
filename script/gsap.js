ac70b5bf81cca35d9be001c73f5b397cb9266bf0
ys7520ys <108736864+ys7520ys@users.noreply.github.com>
2024-12-27 04:25:54 +0900

    Add files via upload

--- script/gsap.js
+++ script/gsap.js
@@ -1,4 +1,4 @@
-// ----------첫 번째 section----------
+// ----------첫 번째 섹션 start----------
 gsap.registerPlugin(ScrollTrigger);
 gsap.fromTo(
 ".firstSection .section-title, .firstSection .section-subtitle, .firstSection .card-title, .firstSection .card-subtitle", 
@@ -48,12 +48,9 @@ gsap.fromTo(
     }
   }
 );
+// ----------첫 번째 섹션 end----------
 
-
-
-
-
-// ----------두 번째 section----------
+// ----------두 번째 섹션 start----------
 gsap.fromTo(
 ".secondSection .text_box", 
 { opacity: 0, x: 100 }, 
@@ -86,12 +83,9 @@ gsap.fromTo(
     }
   }
 );
+// ----------두 번째 섹션 end----------
 
-
-
-
-
-// ----------세 번째 section----------
+// ----------세 번째 섹션 start----------
 gsap.fromTo(
   ".thirdSection .section-title, .thirdSection .section-subtitle", 
   { opacity: 0, y: 100 }, 
@@ -117,8 +111,8 @@ gsap.fromTo(
     duration: 1, 
     scrollTrigger: {
       trigger: ".thirdSection .card_box>div:nth-of-type(1)",
-      start: "top 90%", 
-      end: "top 55%", 
+      start: "top 95%", 
+      end: "top 65%", 
       scrub: 1, 
       markers: false
     }
@@ -133,8 +127,8 @@ gsap.fromTo(
     duration: 1, 
     scrollTrigger: {
       trigger: ".thirdSection .card_box>div:nth-of-type(2)",
-      start: "top 80%",
-      end: "top 55%", 
+      start: "top 85%",
+      end: "top 65%", 
       scrub: 1, 
       markers: false 
     }
@@ -149,47 +143,16 @@ gsap.fromTo(
     duration: 1, 
     scrollTrigger: {
       trigger: ".thirdSection .card_box>div:nth-of-type(3)",
-      start: "top 70%", 
-      end: "top 55%", 
-      scrub: 1, 
-      markers: false 
-    }
-  }
-);
-
-
-
-
-
-
-// ----------배너 section----------
-gsap.fromTo(
-  ".bannerSection .section-title", 
-  { opacity: 0, y: 100, fontSize: 10}, 
-  { 
-    opacity: 1, 
-    y: 0, 
-    duration: 1, 
-    fontSize: 100,
-    scrollTrigger: {
-      trigger: ".bannerSection .section-title",
-      start: "top 67%", 
+      start: "top 75%", 
       end: "top 65%", 
       scrub: 1, 
       markers: false 
     }
   }
 );
+// ----------세 번째 섹션 end----------
 
-
-
-
-
-
-
-
-
-// ----------네 번째 section----------
+// ----------네 번째 섹션 start----------
 gsap.registerPlugin(ScrollTrigger);
 gsap.fromTo(
   "section.fourthSection h2.section-title, section.fourthSection h3.section-subtitle", 
@@ -209,47 +172,25 @@ gsap.fromTo(
   );
 gsap.to(".fourthSection .card:nth-of-type(1), .fourthSection .card:nth-of-type(2), .fourthSection .card:nth-of-type(3)", {
   scrollTrigger: {
-    trigger: ".fourthSection .card:nth-of-type(1), .fourthSection .card:nth-of-type(2), .fourthSection .card:nth-of-type(3)", // 대상 요소
-    start: "top 80%", // 애니메이션 시작 위치
-    end: "top 70%", // 애니메이션 종료 위치
-    scrub: true, // 스크롤에 따라 애니메이션 진행
-    markers: false, // 디버깅용 마커 (테스트 후 삭제 가능)
+    trigger: ".fourthSection .card:nth-of-type(1), .fourthSection .card:nth-of-type(2), .fourthSection .card:nth-of-type(3)",
+    start: "top 80%", 
+    end: "top 70%", 
+    scrub: true, 
+    markers: false,
   },
-  rotateX: 0, // Y축을 기준으로 180도 회전
+  rotateX: 0, 
   opacity: 1
 });
 gsap.to(".fourthSection .card:nth-of-type(4), .fourthSection .card:nth-of-type(5), .fourthSection .card:nth-of-type(6)", {
   scrollTrigger: {
-    trigger: ".fourthSection .card:nth-of-type(4), .fourthSection .card:nth-of-type(5), .fourthSection .card:nth-of-type(6)", // 대상 요소
+    trigger: ".fourthSection .card:nth-of-type(4), .fourthSection .card:nth-of-type(5), .fourthSection .card:nth-of-type(6)", 
     duration: 2,
-    start: "top 80%", // 애니메이션 시작 위치
-    end: "top 70%", // 애니메이션 종료 위치
-    scrub: true, // 스크롤에 따라 애니메이션 진행
-    markers: false, // 디버깅용 마커 (테스트 후 삭제 가능)
+    start: "top 80%", 
+    end: "top 70%", 
+    scrub: true, 
+    markers: false, 
   },
-  rotateX: 0, // Y축을 기준으로 180도 회전
+  rotateX: 0, 
   opacity: 1
 });
-
-
-
-
-
-
-// ----------자동 슬라이드 헤더(사용X)----------
-// const showNav = gsap.from("header", { 
-//   yPercent: -100,
-//   paused: true,
-//   duration: 0.3
-// }).progress(1);
-// ScrollTrigger.create({
-//   start: "top top",
-//   end: 99999,
-//   onUpdate: (self) => {
-//       self.direction === -1 ? showNav.play() : showNav.reverse()
-//   }
-// });
-
-
-
-
+// ----------네 번째 섹션 end----------
