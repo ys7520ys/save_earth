5fdcb66e64e2b320b423dcef1260e83f66f79051
ys7520ys <108736864+ys7520ys@users.noreply.github.com>
2025-01-02 20:09:50 +0900

    Add files via upload

--- script/smoothScroll.js
+++ script/smoothScroll.js
@@ -4,28 +4,28 @@ $('header a, .btn-gototop').click(function(){
 })
 
 // 앵커 체인지 - 각 섹션으로 이동
-gsap.registerPlugin(ScrollToPlugin);
-const sections = document.querySelectorAll(".section");
-let currentIndex = 0;
-let isScrolling = false;
-const scrollToSection = (index) => {
-  if (index < 0 || index >= sections.length) return;
-  isScrolling = true;
-  gsap.to(window, {
-    scrollTo: sections[index],
-    duration: 0.5,
-    ease: "power1.inOut",
-    onComplete: () => {
-      currentIndex = index; 
-      isScrolling = false; 
-    },
-  });
-};
-window.addEventListener("wheel", (event) => {
-  if (isScrolling) return;
-  if (event.deltaY > 0) {
-    scrollToSection(currentIndex + 1);
-  } else {
-    scrollToSection(currentIndex - 1); 
-  }
-});
+// gsap.registerPlugin(ScrollToPlugin);
+// const sections = document.querySelectorAll(".section");
+// let currentIndex = 0;
+// let isScrolling = false;
+// const scrollToSection = (index) => {
+//   if (index < 0 || index >= sections.length) return;
+//   isScrolling = true;
+//   gsap.to(window, {
+//     scrollTo: sections[index],
+//     duration: 0.5,
+//     ease: "power1.inOut",
+//     onComplete: () => {
+//       currentIndex = index; 
+//       isScrolling = false; 
+//     },
+//   });
+// };
+// window.addEventListener("wheel", (event) => {
+//   if (isScrolling) return;
+//   if (event.deltaY > 0) {
+//     scrollToSection(currentIndex + 1);
+//   } else {
+//     scrollToSection(currentIndex - 1); 
+//   }
+// });
